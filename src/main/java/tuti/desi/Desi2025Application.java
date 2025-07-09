80014a22846596a0ef2a154b90a787de20b07927
fkflo <fkfloresferreyra@gmail.com>
2025-07-08 23:47:35 -0300

    Archivos innecesarios eliminados

--- src/main/java/tuti/desi/Desi2025Application.java
+++ src/main/java/tuti/desi/Desi2025Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Desi2025Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Desi2025Application.class, args);
 	}
-
 }
